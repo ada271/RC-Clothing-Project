fc1659d51c392e4aeb4f2e65fbe585c514c3cfdc
ada <h.kaansolmaz@gmail.com>
2024-07-08 11:02:09 +0300

    br

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,18 @@
+import "./Header.scss"
 
-
-const Header = () => {
+const Header = ({categories}) => {
+    //  console.log(categories)
   return (
-    <div>Header</div>
+    <div className="header">
+        <h1>Product List</h1>
+        <div className="btns">
+            {
+                categories.map((a)=>(
+                    <button key={a}>{a}</button>
+                ))
+            }
+        </div>
+    </div>
   )
 }
 
